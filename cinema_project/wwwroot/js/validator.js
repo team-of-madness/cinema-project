b00a45cf000802f4856cd39e4604959da3310faa
Dmytro Manilo <manilodmitrij.02@gmail.com>
2023-04-11 17:48:04 +0300

    Client-side validation on Genres views

---
+++ cinema_project/wwwroot/js/validator.js
@@ -0,0 +1,10 @@
+﻿
+const input = document.getElementById('onlyLettersField'); // отримання елементу вводу
+const regex = /^[a-zA-Z\s]*$/; // регулярний вираз, який відповідає лише літерам та пробілам
+
+input.addEventListener('input', () => { // додавання обробника події для вводу
+    const value = input.value; // отримання значення вводу
+    if (!regex.test(value)) { // перевірка, чи відповідає регулярному виразу
+        input.value = value.replace(/[^a-zA-Z\s]/g, ''); // видалення неприпустимих символів зі значення
+    }
+});
